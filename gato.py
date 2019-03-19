f1a6dff468244fd0bbd455877eaed04b805cc4b0
mike9739 <migu_el_39@hotmail.com>
2019-03-19 09:07:09 -0600

    Juego de gato, utiliza un algoritmo sencillo -si hay una posibilidad de ganar realizar la jugada -si eljugador contrario tiene posibilidad de ganar , bloquea esa posibilidad - Siempre buscar si una esquina esta libre - despues buscar el centro del tablero -Si hay varas esquinas seleccionar una aleatoriamente - si ya no hya mas movimientos se declara un empate

---
+++ gato.py
@@ -0,0 +1,128 @@
+#crea una lista con las posiciones para el tablero
+board = [' ' for x in range(10)]
+#funcionoara insetar un simbolo , recibe un simbolo X/O y una posicion POS
+def insertLetter(letter, pos):
+    board[pos] = letter
+#verifica si el espacio esta libre
+def spaceIsFree(pos):
+    return board[pos] == ' '
+#dibujar el tablero
+def printBoard(board):
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+ #verifica si hay un ganador   
+def isWinner(bo, le):
+    #sistemas de reglas :D
+    return (bo[7] == le and bo[8] == le and bo[9] == le) or (bo[4] == le and bo[5] == le and bo[6] == le) or(bo[1] == le and bo[2] == le and bo[3] == le) or(bo[1] == le and bo[4] == le and bo[7] == le) or(bo[2] == le and bo[5] == le and bo[8] == le) or(bo[3] == le and bo[6] == le and bo[9] == le) or(bo[1] == le and bo[5] == le and bo[9] == le) or(bo[3] == le and bo[5] == le and bo[7] == le)
+
+def playerMove():
+    run = True
+    while run:
+        move = input('Por favor seleccione la posicion donde quiera poner una \'X\' (1-9): ')
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertLetter('X', move)
+                else:
+                    print('El lugar ya esta ocupado, por favor intente otra posicion!')
+            else:
+                print('Por favor seleccione un numero dentro del rango!')
+        except:
+            print('Por favor escriba un numero!')
+            
+
+def compMove():
+    #genera una lista con los posibles movimientos
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
+    move = 0
+    #Revisa si la maquina puede ganar o el jugafor va a ganar
+    for let in ['O', 'X']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = let
+            if isWinner(boardCopy, let):
+                move = i
+                return move
+    #trata de tomar las esquinas
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1,3,7,9]:
+            cornersOpen.append(i)
+            
+    if len(cornersOpen) > 0:
+        move = selectRandom(cornersOpen)
+        return move
+    #trata de ganar el centro
+    if 5 in possibleMoves:
+        move = 5
+        return move
+    #trata de ganar los bordes
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2,4,6,8]:
+            edgesOpen.append(i)
+            
+    if len(edgesOpen) > 0:
+        move = selectRandom(edgesOpen)
+        
+    return move
+    #selecciona una jugada aleatoriamente 
+def selectRandom(li):
+    import random
+    ln = len(li)
+    r = random.randrange(0,ln)
+    return li[r]
+    
+
+def isBoardFull(board):
+    if board.count(' ') > 1:
+        return False
+    else:
+        return True
+
+def main():
+    print('Vamos a jugar al gato :D')
+    printBoard(board)
+
+    while not(isBoardFull(board)):
+        if not(isWinner(board, 'O')):
+            playerMove()
+            printBoard(board)
+        else:
+            print('La computadora te gano :C')
+            break
+
+        if not(isWinner(board, 'X')):
+            move = compMove()
+            if move == 0:
+                print('Empate :/!')
+            else:
+                insertLetter('O', move)
+                print('La compputadora coloco una O en:', move , ':')
+                printBoard(board)
+        else:
+            print('Le ganastesss! :D')
+            break
+
+    if isBoardFull(board):
+        print('Empate!')
+
+while True:
+    answer = input('Quieres volver a jugar? (Y/N)')
+    if answer.lower() == 'y' or answer.lower == 'yes':
+        board = [' ' for x in range(10)]
+        print('-----------------------------------')
+        main()
+    else:
+        break
